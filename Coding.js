715217cb2be2c92f373e748096e8c67ffe807d28
samuelRiad <118536585+samuelRiad@users.noreply.github.com>
2022-11-18 14:36:49 +0200

    index.html

---
+++ Coding.js
@@ -0,0 +1,24 @@
+function st() {
+  alert("master of puppets , california dreamin' , kids , Never ending story ");
+}
+function hp() {
+  alert(
+    "Double Trouble, In Noctem , Proffesor Umbridge , A window to the past"
+  );
+}
+function lr() {
+  alert("The Shire, Rohan , Elanor");
+}
+function bb() {
+  alert("Little Saint Nick , Surffin' USA , California Dreamin' ");
+}
+function m() {
+  alert(
+    "Nothing else matter , The Unforgiven , Fade to black , Master of puppets"
+  );
+}
+function $() {
+  alert(
+    "Master of Puppets , Psycho Killer , California Dreamin' , Double Trouble "
+  );
+}
